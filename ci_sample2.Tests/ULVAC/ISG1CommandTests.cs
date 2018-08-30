4f2afa8d5d716d08b8566dbc274445f6498bdca8
seki <hiroyuki.seki@horiba.com>
2018-08-30 18:42:34 +0900

    Added ULVAC ISG1Command class and test

---
+++ ci_sample2.Tests/ULVAC/ISG1CommandTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using STECCommon.Equipment.ULVAC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace STECCommon.Equipment.ULVAC.Tests
+{
+    [TestClass()]
+    public class ISG1CommandTests
+    {
+        [TestMethod()]
+        public void CalcChecksumTest()
+        {
+            string checkSum = ISG1Command.CalcChecksum(":11D");
+
+            Assert.AreEqual("44", checkSum);
+
+            //  取説の例に間違いあり、注意
+            checkSum = ISG1Command.CalcChecksum(":11D4.00E05F6");
+
+            Assert.AreEqual("6E", checkSum);
+
+            checkSum = ISG1Command.CalcChecksum(":01n");
+
+            Assert.AreEqual("6F", checkSum);
+        }
+
+        [TestMethod()]
+        public void ISG1CommandTest()
+        {
+            var command = new ISG1Command(11, "D");
+
+            Assert.AreEqual(":11D44", command.GetCommandString());
+        }
+
+        [TestMethod()]
+        public void ISG1CommandTest1()
+        {
+            var command = new ISG1Command(":11D4.00E05F66E");
+
+            Assert.AreEqual(11, command.Address);
+            Assert.AreEqual("D4.00E05F6", command.Parameter);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ISG1CommandTest2()
+        {
+            //  アドレス解釈失敗
+            var command = new ISG1Command(":1BD4.00E05F66E");
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ISG1CommandTest3()
+        {
+            //  チェックサム異常
+            var command = new ISG1Command(":11D4.00E05F660");
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ISG1CommandTest4()
+        {
+            //  コマンド長不足
+            var command = new ISG1Command(":01AA");
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ISG1CommandTest5()
+        {
+            //  チェックサム異常(レスポンスのパラメータがn）
+            var command = new ISG1Command(":01n6F");
+        }
+
+
+    }
+}
---
+++ ci_sample2/ULVAC/ISG1Command.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace STECCommon.Equipment.ULVAC
+{
+    public class ISG1Command
+    {
+        /// <summary>
+        /// ヘッダー
+        /// </summary>
+        static char HEADER = (char)':';
+
+        /// <summary>
+        /// アドレス
+        /// </summary>
+        public int Address { get; private set; }
+
+        /// <summary>
+        /// パラメータ
+        /// </summary>
+        public string Parameter { get; private set; }
+
+        /// <summary>
+        /// 送信用コマンドを作成する
+        /// </summary>
+        /// <param name="address"></param>
+        /// <param name="parameter"></param>
+        public ISG1Command(int address, string parameter)
+        {
+            Address = address;
+            Parameter = parameter;
+        }
+
+        /// <summary>
+        /// 受信したコマンドを解釈する
+        /// チェックサムが異なる場合、例外が発生する
+        /// </summary>
+        /// <param name="rawResponse"></param>
+        public ISG1Command(string rawResponse)
+        {
+            string endTrimmedResponse = rawResponse.TrimEnd();
+
+            //  コマンド長を確認する
+            //  :ADxCCの6文字はないとおかしい
+            if(endTrimmedResponse.Length < 6)
+            {
+                throw new InvalidOperationException($"ISG1 command too short ({endTrimmedResponse})");
+            }
+
+            //  アドレスを納める
+            string addressStr = endTrimmedResponse.Substring(1, 2);
+            int address = 0;
+            bool parseOk = int.TryParse(addressStr, out address);
+            if(parseOk == false)
+            {
+                throw new InvalidOperationException($"ISG1 command address parse error ({addressStr})");
+            }
+            Address = address;
+
+            //  パラメータを納める
+            Parameter = endTrimmedResponse.Substring(3, endTrimmedResponse.Length - 3 - 2);
+
+            //  受信したコマンドのチェックサムを確認する
+            string responseCheckSum = endTrimmedResponse.Substring(rawResponse.Length - 2, 2);
+            string calcedCheckSum = CalcChecksum(endTrimmedResponse.Substring(0, rawResponse.Length - 2));
+
+            if(responseCheckSum != calcedCheckSum)
+            {
+                throw new InvalidOperationException($"ISG1 command checksum error");
+            }
+
+            //  送信したコマンドのチェックサムに問題が合った場合、nが返される
+            if(Parameter == "n")
+            {
+                throw new InvalidOperationException("ISG1 command checksum error response \"n\"");
+            }
+        }
+
+        /// <summary>
+        /// 送受信用の文字列を返す
+        /// </summary>
+        /// <returns></returns>
+        public string GetCommandString()
+        {
+            StringBuilder commandString = new StringBuilder();
+
+            commandString.Append(HEADER);
+            commandString.Append(Address.ToString("00"));
+            commandString.Append(Parameter);
+            commandString.Append(CalcChecksum(commandString.ToString()));
+
+            return commandString.ToString();
+        }
+
+        /// <summary>
+        /// チェックサムの文字列を算出する
+        /// </summary>
+        /// <param name="headerAddressParameter"></param>
+        /// <returns></returns>
+        public static string CalcChecksum(string headerAddressParameter)
+        {
+            //  ヘッダーの:はチェックサム計算に含めない
+            string addressParameter = headerAddressParameter.Remove(0, 1);
+
+            byte checkSum = (byte)addressParameter[0];
+
+            foreach (var c in addressParameter.Skip(1))
+            {
+                checkSum = (byte)(checkSum ^ (byte)c);
+            }
+
+            return checkSum.ToString("X2");
+        }
+    }
+}
